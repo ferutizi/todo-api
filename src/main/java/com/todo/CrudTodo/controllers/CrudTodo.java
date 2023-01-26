23f751a01cf0cd3a6901968aafc43506cbca738b
ferutizi <fer.utizi@gmail.com>
2023-01-26 19:27:04 -0300

    method en requestMapping, CrossOrigin

--- src/main/java/com/todo/CrudTodo/controllers/CrudTodo.java
+++ src/main/java/com/todo/CrudTodo/controllers/CrudTodo.java
@@ -5,26 +5,30 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.todo.CrudTodo.models.TodoModel;
 import com.todo.CrudTodo.services.TodoService;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping(value = "/", method={RequestMethod.POST, RequestMethod.GET})
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 public class CrudTodo {
     @Autowired
     TodoService todoService;
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public ArrayList<TodoModel> obtenerTodos(){
         return todoService.obtenerTodos();
     }
 
-    @PostMapping()
+    @PostMapping(value = "/guardar", consumes = {"application/json"})
     public TodoModel guardarTodo(@RequestBody TodoModel todo){
+        System.out.println(todo);
         return this.todoService.guardarTodo(todo);
     }
 }
