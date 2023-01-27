d366e31e22d0d60a84a31171449a0bb95e40cc93
ferutizi <fer.utizi@gmail.com>
2023-01-27 19:52:10 -0300

    deleteMapping

--- src/main/java/com/todo/CrudTodo/controllers/CrudTodo.java
+++ src/main/java/com/todo/CrudTodo/controllers/CrudTodo.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,7 +17,7 @@ import com.todo.CrudTodo.models.TodoModel;
 import com.todo.CrudTodo.services.TodoService;
 
 @RestController
-@RequestMapping(value = "/", method={RequestMethod.POST, RequestMethod.GET})
+@RequestMapping(value = "/", method={RequestMethod.POST, RequestMethod.GET, RequestMethod.DELETE})
 @CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 public class CrudTodo {
     @Autowired
@@ -30,5 +32,10 @@ public class CrudTodo {
     public TodoModel guardarTodo(@RequestBody TodoModel todo){
         System.out.println(todo);
         return this.todoService.guardarTodo(todo);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void eliminarTodo(@PathVariable("id") Long id) {
+        todoService.eliminarTodo(id);
     }
 }
